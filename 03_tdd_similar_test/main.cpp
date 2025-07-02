378386dae018dcf7323c9fdf9ec69f416baa3a75
Taehyun <ohyeaha@hanmail.net>
2025-07-02 15:28:02 +0900

    [feature] length - 1

--- 03_tdd_similar_test/main.cpp
+++ 03_tdd_similar_test/main.cpp
@@ -1,6 +1,14 @@
 #include "gmock/gmock.h"
 #include "similarity-checker.cpp"
 
+TEST(SimilarityChecker, LengthSame) {
+    SimilarityChecker sc{ "ASD" };
+
+    int actual = sc.check("DSA");
+
+    EXPECT_EQ(60, actual);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
--- 03_tdd_similar_test/similarity-checker.cpp
+++ 03_tdd_similar_test/similarity-checker.cpp
@@ -1,5 +1,18 @@
+#include <string>
+
+using namespace std;
 
 class SimilarityChecker {
 public:
+    SimilarityChecker(string solution) : solution{ solution } {}
 
+    int check(const string& input) {
+        if (input.length() == solution.length()) {
+            return 60;
+        }
+        return 0;
+    }
+
+private:
+    string solution;
 };
