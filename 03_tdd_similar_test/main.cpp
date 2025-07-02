98e20c61d026b05e32281c280baa2602ad1f389e
Taehyun <ohyeaha@hanmail.net>
2025-07-02 15:44:37 +0900

    [feature] apply length double

--- 03_tdd_similar_test/main.cpp
+++ 03_tdd_similar_test/main.cpp
@@ -17,6 +17,30 @@ TEST(SimilarityChecker, LengthSame2) {
     EXPECT_EQ(60, actual);
 }
 
+TEST(SimilarityChecker, LengthDouble) {
+    SimilarityChecker sc{ "A" };
+
+    double actual = sc.check("BB");
+
+    EXPECT_EQ(0, actual);
+}
+
+TEST(SimilarityChecker, LengthDouble2) {
+    SimilarityChecker sc{ "AA" };
+
+    double actual = sc.check("BBBBB");
+
+    EXPECT_EQ(0, actual);
+}
+
+TEST(SimilarityChecker, LengthDouble3) {
+    SimilarityChecker sc{ "CCCAAAAA" };
+
+    double actual = sc.check("ED");
+
+    EXPECT_EQ(0, actual);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
--- 03_tdd_similar_test/similarity-checker.cpp
+++ 03_tdd_similar_test/similarity-checker.cpp
@@ -11,8 +11,13 @@ public:
         int sl = solution.length();
         int il = input.length();
 
+        if (sl >= il * 2 || il >= sl * 2) {
+            return 0;
+        }
+
         int gap = 0;
         double score = 0;
+
         if (sl > il) {
             gap = sl - il;
             score = (1 - gap / il) * 60.0;
